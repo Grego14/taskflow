d0a802a9d9a0fbcd7cea9e5379893daca1361ca5
Gregorio <gre208981@gmail.com>
2025-06-30 23:49:57 -0400

    Add Router

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './main.css'
 import { AuthProvider } from './firebase/AuthContext'
 import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 history.scrollRestoration = 'manual'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </AuthProvider>
   </StrictMode>
 )
