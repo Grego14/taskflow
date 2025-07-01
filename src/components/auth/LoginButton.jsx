d6c1335d1fb9573cfdd6be5950372ef846a4afac
Gregorio <gre208981@gmail.com>
2025-06-30 23:52:11 -0400

    Move buttons to components/reusable/buttons

--- src/components/auth/LoginButton.jsx
+++
@@ -1,5 +0,0 @@
-import navigate from '@utils/navigate'
-
-export default function LoginButton() {
-  return <button onClick={() => navigate('/login')}>Login</button>
-}
--- src/components/auth/SignUpButton.jsx
+++
@@ -1,5 +0,0 @@
-import navigate from "../../utils/navigate";
-
-export default function SignUpButton() {
-  return <button onClick={() => navigate('signup')}>Sign Up</button>
-}
