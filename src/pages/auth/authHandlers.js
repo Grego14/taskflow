5b20184c5961d7e4736bb920a62e19be6de03b7c
Gregorio <gre208981@gmail.com>
2025-07-14 00:47:02 -0400

    Remove console logs and errors and implement use of the getFriendlyAuthError utility function;

--- src/pages/auth/authHandlers.js
+++ src/pages/auth/authHandlers.js
@@ -1,37 +1,28 @@
 import { signIn, signUp } from '@/firebase/auth.js'
 import { updateProfile } from 'firebase/auth'
+import getFriendlyAuthError from '@utils/getFriendlyAuthError'
 
-async function login(email, password) {
-  console.log('Email %s; Password %s', email, password)
-
+async function login({ email, password }) {
   try {
-    const loginData = await signIn(email, password)
+    const user = await signIn(email, password)
 
-    if (loginData) {
-      console.log('Logged successfully', loginData)
-    }
-
-    return loginData
-  } catch (error) {
-    console.error('Log In Error: ', error.message)
+    if (user) return user
+  } catch (err) {
+    throw getFriendlyAuthError(err.message)
   }
 }
 
-async function signup(username, email, password) {
-  console.log('Username %s; Email %s; Password %s', username, email, password)
+async function signup({ username, email, password }) {
   try {
-    const signUpData = await signUp(email, password)
+    const user = await signUp(email, password)
 
-    if (signUpData) {
-      console.log('Account created successfully.', signUpData)
-
-      // Change the auth.displayName to "username"
-      await updateProfile(signUpData, { displayName: username })
+    if (user) {
+      await updateProfile(user, { displayName: username })
+      return user
     }
-
-    return signUpData
-  } catch (error) {
-    console.error('Sign Up Error: ', error.message)
+  } catch (err) {
+    console.error(err)
+    throw getFriendlyAuthError(err.message)
   }
 }
 
