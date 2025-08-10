70470e17fefccd9ad884d85af779b8e124214a32
Gregorio <gre208981@gmail.com>
2025-08-10 15:58:20 -0400

    Update createTask service params and now createdAt, updatedAt and createdBy fields are now being added in this service

--- src/services/createTask.js
+++ src/services/createTask.js
@@ -1,40 +1,33 @@
 import { db } from '@/firebase/firebase-config.js'
 import getFriendlyAuthError from '@utils/getFriendlyAuthError'
-import { addDoc, collection, doc, serverTimestamp } from 'firebase/firestore'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
 
 /**
  * creates a project task
- * @param {string} userId - ID of the user
- * @param {string} projectId - ID of the project
- * @param {string} task - Data of the task
+ * @param {string} user - ID of the user
+ * @param {string} project - ID of the project
+ * @param {string} data - Data of the task
  * @throws A getFriendlyAuthError object if the data is not found: {code: string, message: string}
  */
-export default async function createTask(userId, projectId, task) {
-  if (!userId || typeof userId !== 'string')
-    throw Error('createTask error: Invalid userId')
+export default async function createTask({ user, project, data }) {
+  if (!user || typeof user !== 'string')
+    throw Error('createTask error: Invalid user id')
 
-  if (!projectId || typeof projectId !== 'string')
-    throw Error('createTask error: Invalid projectId')
+  if (!project || typeof project !== 'string')
+    throw Error('createTask error: Invalid project id')
 
-  if (!task || typeof task !== 'object' || Object.keys(task).length === 0)
+  if (!data || typeof data !== 'object' || Object.keys(data).length === 0)
     throw Error('createTask error: No task data provided!')
 
   try {
-    const taskCol = collection(
-      db,
-      'users',
-      userId,
-      'projects',
-      projectId,
-      'tasks'
-    )
-    const projectRef = doc(db, 'users', userId, 'projects', projectId)
+    const taskRef = collection(db, 'users', user, 'projects', project, 'tasks')
 
-    await addDoc(taskCol, {
-      ...task,
-      projectId: projectRef.id,
+    await addDoc(taskRef, {
+      ...data,
+      id: taskRef.id,
       createdAt: serverTimestamp(),
-      updatedAt: serverTimestamp()
+      updatedAt: serverTimestamp(),
+      createdBy: user
     })
   } catch (err) {
     console.error('createTask error:', err)
