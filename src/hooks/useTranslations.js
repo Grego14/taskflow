9dcbd28e5558e7d2393d387378e61d1d29b52688
Gregorio <gre208981@gmail.com>
2025-11-22 20:53:12 -0400

    Remove useTranslations hooks and add the new dependencies

--- src/hooks/useTranslations.js
+++
@@ -1,8 +0,0 @@
-import { useAppState } from '@/context/AppContext'
-import translations from '@/translations.js'
-
-export default function useTranslations() {
-  const { user } = useAppState()
-
-  return translations[user.preferences.lang]
-}
