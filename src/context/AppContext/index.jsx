c14c0869403aedef0a66086836218677fafca435
Gregorio <gre208981@gmail.com>
2025-11-30 00:38:21 -0400

    Choose when to render the routes that use react-router or the landing page

--- src/context/AppContext/index.jsx
+++ src/context/AppContext/index.jsx
@@ -1,6 +1,8 @@
 // hooks
 import useMediaQuery from '@mui/material/useMediaQuery'
-import { useCallback, useMemo, useReducer, useState } from 'react'
+import { useCallback, useMemo, useReducer, useState, lazy } from 'react'
+import { useColorScheme } from '@mui/material/styles'
+import useAuth from '@hooks/useAuth.js'
 
 // utils
 import {
@@ -9,12 +11,18 @@ import {
   initAppState
 } from './appStateReducer.js'
 import AppContext from './context'
+import UserContext from '@context/UserContext'
+
+const AppRoutes = lazy(() => import('@/AppRoutes.jsx'))
+const Landing = lazy(() => import('@pages/home/Landing'))
 
 export default function AppProvider({ children }) {
+  const { currentUser } = useAuth()
   const [appState, dispatch] = useReducer(appStateReducer, {}, initAppState)
   const isMobile = useMediaQuery(theme => theme.breakpoints.down('tablet'))
   const isOnlyMobile = useMediaQuery(theme => theme.breakpoints.down('mobile'))
   const [lastRute, setLastRute] = useState('/')
+  const { mode } = useColorScheme()
 
   // used to manage the alignment of the AppDrawer and the AppBar
   const appBarHeight = isMobile ? '3.8rem' : '3.4rem'
@@ -41,7 +49,16 @@ export default function AppProvider({ children }) {
     [appState, appBarHeight, isMobile, isOnlyMobile, appNotification, lastRute]
   )
 
+  const isAuthRute =
+    location.pathname === '/login' || location.pathname === '/signup'
+
+  if (!mode) return null
+
   return (
-    <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
+    <AppContext.Provider value={contextValue}>
+      <UserContext>
+        {!currentUser && !isAuthRute ? <Landing /> : <AppRoutes />}
+      </UserContext>
+    </AppContext.Provider>
   )
 }
