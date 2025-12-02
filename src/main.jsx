95ead9fb786560b009788ce917da2581e8951fbf
Gregorio <gre208981@gmail.com>
2025-12-01 22:30:08 -0400

    Add: Main component and do not query the fonts before adding them to the preload

--- src/main.jsx
+++ src/main.jsx
@@ -12,157 +12,174 @@ import AppProvider from '@context/AppContext'
 import AuthProvider from '@context/AuthContext'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './i18n.js'
 
 import { createTheme, responsiveFontSizes } from '@mui/material/styles'
 
-import RubikBold from '/fonts/Rubik-Bold.woff2'
-import RubikMedium from '/fonts/Rubik-Medium.woff2'
-import RubikRegular from '/fonts/Rubik-Regular.woff2'
 import './main.css'
 
 history.scrollRestoration = 'manual'
 
-const theme = responsiveFontSizes(
-  createTheme({
-    colorSchemes: {
-      light: {
-        palette: {
-          primary: {
-            main: '#54448A'
-          },
-          secondary: {
-            main: '#B97BD8'
-          }
-        }
-      },
-      dark: {
-        palette: {
-          primary: {
-            main: '#A592E8'
-          },
-          secondary: {
-            main: '#D29AEE'
-          }
-        }
-      }
-    },
-    typography: {
-      fontFamily: 'Rubik, Arial, sans-serif'
-    },
-    components: {
-      MuiButton: {
-        styleOverrides: {
-          root: ({ theme, ...props }) => {
-            const defaultTransition = theme.transitions.create(
-              [
-                'background-color',
-                'box-shadow',
-                'border-color',
-                'color',
-                'scale'
-              ],
-              {
-                duration: theme.transitions.duration.short
-              }
-            )
+const fontsToPreload = {
+  bold: '/fonts/Rubik-Bold.woff2',
+  medium: '/fonts/Rubik-Medium.woff2',
+  regular: '/fonts/Rubik-Regular.woff2'
+}
 
-            return {
-              fontSize: 'var(--fs-small)',
-              textTransform: 'none',
-              minWidth: 'auto',
-              transition: defaultTransition,
-              '& .MuiButton-icon': { transition: 'inherit' },
-              // add a cool press effect
-              '&:active, &:active .MuiButton-icon': {
-                scale: 0.95
-              }
+function Main() {
+  useEffect(() => {
+    for (const font of Object.values(fontsToPreload)) {
+      const link = document.createElement('link')
+
+      link.rel = 'preload'
+      link.as = 'font'
+      link.href = font
+      link.type = 'font/woff2'
+      link.crossOrigin = 'anonymous'
+
+      document.head.appendChild(link)
+    }
+  }, [])
+
+  const theme = responsiveFontSizes(
+    createTheme({
+      colorSchemes: {
+        light: {
+          palette: {
+            primary: {
+              main: '#54448A'
+            },
+            secondary: {
+              main: '#B97BD8'
+            }
+          }
+        },
+        dark: {
+          palette: {
+            primary: {
+              main: '#A592E8'
+            },
+            secondary: {
+              main: '#D29AEE'
             }
           }
         }
       },
-      MuiTooltip: {
-        defaultProps: {
-          slots: { transition: Zoom },
-          arrow: true,
-          enterDelay: 350
-        }
+      typography: {
+        fontFamily: 'Rubik, Arial, sans-serif'
       },
-      MuiFormHelperText: {
-        styleOverrides: {
-          root: {
-            // remove the margin at the sides so the helper text can be
-            // aligned with the input
-            marginInline: 0
-          }
-        }
-      },
-      MuiSvgIcon: {
-        styleOverrides: {
-          root: ({ theme, ...props }) => {
-            const { color, ownerState } = props
-            const providedColor = color || ownerState.color
+      components: {
+        MuiButton: {
+          styleOverrides: {
+            root: ({ theme, ...props }) => {
+              const defaultTransition = theme.transitions.create(
+                [
+                  'background-color',
+                  'box-shadow',
+                  'border-color',
+                  'color',
+                  'scale'
+                ],
+                {
+                  duration: theme.transitions.duration.short
+                }
+              )
 
-            return {
-              color: providedColor || theme.palette.primary.main
+              return {
+                fontSize: 'var(--fs-small)',
+                textTransform: 'none',
+                minWidth: 'auto',
+                transition: defaultTransition,
+                '& .MuiButton-icon': { transition: 'inherit' },
+                // add a cool press effect
+                '&:active, &:active .MuiButton-icon': {
+                  scale: 0.95
+                }
+              }
             }
           }
-        }
-      },
-      MuiCssBaseline: {
-        styleOverrides: `
+        },
+        MuiTooltip: {
+          defaultProps: {
+            slots: { transition: Zoom },
+            arrow: true,
+            enterDelay: 350
+          }
+        },
+        MuiFormHelperText: {
+          styleOverrides: {
+            root: {
+              // remove the margin at the sides so the helper text can be
+              // aligned with the input
+              marginInline: 0
+            }
+          }
+        },
+        MuiSvgIcon: {
+          styleOverrides: {
+            root: ({ theme, ...props }) => {
+              const { color, ownerState } = props
+              const providedColor = color || ownerState.color
+
+              return {
+                color: providedColor || theme.palette.primary.main
+              }
+            }
+          }
+        },
+        MuiCssBaseline: {
+          styleOverrides: `
         @font-face {
           font-family: 'Rubik';
-          src: local(Rubik), local(Rubik-Regular), url(${RubikRegular}) format('woff2');
+          src: local(Rubik-Bold), url(${fontsToPreload.bold}) format('woff2');
           font-style: normal;
-          font-weight: 400;
+          font-weight: 700;
           font-display: swap;
         }
         @font-face {
           font-family: 'Rubik';
-          src: local(Rubik-Medium), url(${RubikMedium}) format('woff2');
+          src: local(Rubik-Medium), url(${fontsToPreload.medium}) format('woff2');
           font-style: normal;
           font-weight: 500;
           font-display: swap;
         }
         @font-face {
           font-family: 'Rubik';
-          src: local(Rubik-Bold), url(${RubikBold}) format('woff2');
+          src: local(Rubik), local(Rubik-Regular), url(${fontsToPreload.regular}) format('woff2');
           font-style: normal;
-          font-weight: 700;
+          font-weight: 400;
           font-display: swap;
         }
         body{
           font-size: inherit;
         }
       `
-      },
-      MUITypography: {
-        styleOverrides: {
-          root: {
-            fontFamily: 'Rubik, Arial, sans-serif'
+        },
+        MUITypography: {
+          styleOverrides: {
+            root: {
+              fontFamily: 'Rubik, Arial, sans-serif'
+            }
           }
         }
+      },
+      breakpoints: {
+        values: {
+          mobile: 420,
+          tablet: 640,
+          laptop: 1024,
+          desktop: 1360
+        }
       }
-    },
-    breakpoints: {
-      values: {
-        mobile: 420,
-        tablet: 640,
-        laptop: 1024,
-        desktop: 1360
-      }
+    }),
+    {
+      breakpoints: ['mobile', 'tablet', 'laptop', 'desktop']
     }
-  }),
-  {
-    breakpoints: ['mobile', 'tablet', 'laptop', 'desktop']
-  }
-)
+  )
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  return (
     <ThemeProvider noSsr theme={theme}>
       <CssBaseline />
       <I18nextProvider i18n={i18n}>
@@ -171,5 +188,11 @@ createRoot(document.getElementById('root')).render(
         </AuthProvider>
       </I18nextProvider>
     </ThemeProvider>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Main />
   </StrictMode>
 )
