67992d28c4a59cb0bae3648f41e99726db4d0f43
Gregorio <gre208981@gmail.com>
2025-12-06 12:18:35 -0400

    Update: send the user to the /home after he authenticates

--- src/components/reusable/RestrictedRute.jsx
+++ src/components/reusable/RestrictedRute.jsx
@@ -1,14 +1,17 @@
 import { useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { Outlet } from 'react-router-dom'
+import useAuth from '@hooks/useAuth'
 
 export default function RestrictedRute({ isAuthenticated, restrictedPaths }) {
+  const { currentUser } = useAuth()
   const navigate = useNavigate()
   const currentPath = useLocation().pathname
 
   useEffect(() => {
-    if (isAuthenticated && restrictedPaths.includes(currentPath)) navigate('/')
-  }, [restrictedPaths, navigate, isAuthenticated, currentPath])
+    if (isAuthenticated && restrictedPaths.includes(currentPath))
+      navigate(currentUser ? '/home' : '/')
+  }, [restrictedPaths, navigate, isAuthenticated, currentPath, currentUser])
 
   return <Outlet />
 }
