9af34644f21c44fea0401b4d3cb8594892974676
Gregorio <gre208981@gmail.com>
2026-01-08 18:00:09 -0400

    Add the languageDetector i18n plugin again

--- src/i18n.js
+++ src/i18n.js
@@ -1,16 +1,19 @@
 import { loadResources } from '@utils/resources.js'
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 // preload the RESOURCES before calling loadResources to avoid the lexical
 // declaration error
 import { RESOURCES } from './constants'
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init(
     {
       defaultNS: ['common'],
+      lng: i18n.language,
       fallbackLng: 'en',
       interpolation: {
         escapeValue: false
