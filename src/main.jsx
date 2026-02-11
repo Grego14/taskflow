9fba538e316378a0cead4636fc5100a88f7e4cf4
Gregorio <gre208981@gmail.com>
2026-02-10 20:30:56 -0400

    Refactor the main file

--- src/main.jsx
+++ src/main.jsx
@@ -12,195 +12,155 @@ import './main.css'
 
 history.scrollRestoration = 'manual'
 
-const fontsToPreload = {
-  bold: '/fonts/Rubik-Bold.woff2',
-  medium: '/fonts/Rubik-Medium.woff2',
-  regular: '/fonts/Rubik-Regular.woff2'
-}
-
 if (import.meta.env.DEV) {
   import('react-scan').then(({ scan }) => {
     scan({ enabled: true })
   })
 }
 
-function Main() {
-  useEffect(() => {
-    for (const font of Object.values(fontsToPreload)) {
-      const link = document.createElement('link')
-
-      link.rel = 'preload'
-      link.as = 'font'
-      link.href = font
-      link.type = 'font/woff2'
-      link.crossOrigin = 'anonymous'
-
-      document.head.appendChild(link)
-    }
-  }, [])
-
-  const theme = responsiveFontSizes(
-    createTheme({
-      colorSchemes: {
-        light: {
-          palette: {
-            primary: {
-              main: '#7C5DFA'
-            },
-            secondary: {
-              main: '#A592E8'
-            }
+const theme = responsiveFontSizes(
+  createTheme({
+    colorSchemes: {
+      light: {
+        palette: {
+          primary: {
+            main: '#7C5DFA'
+          },
+          secondary: {
+            main: '#A592E8'
           }
-        },
-        dark: {
-          palette: {
-            primary: {
-              main: '#A592E8'
-            },
-            secondary: {
-              main: '#D29AEE'
+        }
+      },
+      dark: {
+        palette: {
+          primary: {
+            main: '#A592E8'
+          },
+          secondary: {
+            main: '#D29AEE'
+          }
+        }
+      }
+    },
+    typography: {
+      fontFamily: 'Rubik, Arial, sans-serif'
+    },
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: ({ theme, ...props }) => {
+            const defaultTransition = theme.transitions.create(
+              [
+                'background-color',
+                'box-shadow',
+                'border-color',
+                'color',
+                'scale'
+              ],
+              {
+                duration: theme.transitions.duration.short
+              }
+            )
+
+            return {
+              fontSize: 'var(--fs-small)',
+              textTransform: 'none',
+              minWidth: 'auto',
+              transition: defaultTransition,
+              '& .MuiButton-icon': { transition: 'inherit' },
+              // add a cool press effect
+              '&:active, &:active .MuiButton-icon': {
+                scale: 0.95
+              }
             }
           }
         }
       },
-      typography: {
-        fontFamily: 'Rubik, Arial, sans-serif'
+      MuiTooltip: {
+        defaultProps: {
+          slots: { transition: Zoom },
+          arrow: true,
+          enterDelay: 350
+        }
       },
-      components: {
-        MuiButton: {
-          styleOverrides: {
-            root: ({ theme, ...props }) => {
-              const defaultTransition = theme.transitions.create(
-                [
-                  'background-color',
-                  'box-shadow',
-                  'border-color',
-                  'color',
-                  'scale'
-                ],
-                {
-                  duration: theme.transitions.duration.short
-                }
-              )
-
-              return {
-                fontSize: 'var(--fs-small)',
-                textTransform: 'none',
-                minWidth: 'auto',
-                transition: defaultTransition,
-                '& .MuiButton-icon': { transition: 'inherit' },
-                // add a cool press effect
-                '&:active, &:active .MuiButton-icon': {
-                  scale: 0.95
-                }
-              }
-            }
+      MuiFormHelperText: {
+        styleOverrides: {
+          root: {
+            // remove the margin at the sides so the helper text can be
+            // aligned with the input
+            marginInline: 0
           }
-        },
-        MuiTooltip: {
-          defaultProps: {
-            slots: { transition: Zoom },
-            arrow: true,
-            enterDelay: 350
-          }
-        },
-        MuiFormHelperText: {
-          styleOverrides: {
-            root: {
-              // remove the margin at the sides so the helper text can be
-              // aligned with the input
-              marginInline: 0
-            }
-          }
-        },
-        MuiSvgIcon: {
-          styleOverrides: {
-            root: ({ theme, ...props }) => {
-              const { color, ownerState } = props
-              const providedColor = color || ownerState.color
-
-              return {
-                color: providedColor || theme.palette.primary.main
-              }
-            }
-          }
-        },
-        MuiCssBaseline: {
-          styleOverrides: `
-        @font-face {
-          font-family: 'Rubik';
-          src: local(Rubik-Bold), url(${fontsToPreload.bold}) format('woff2');
-          font-style: normal;
-          font-weight: 700;
-          font-display: swap;
         }
-        @font-face {
-          font-family: 'Rubik';
-          src: local(Rubik-Medium), url(${fontsToPreload.medium}) format('woff2');
-          font-style: normal;
-          font-weight: 500;
-          font-display: swap;
+      },
+      MuiSvgIcon: {
+        styleOverrides: {
+          root: ({ theme, ...props }) => {
+            const { color, ownerState } = props
+            const providedColor = color || ownerState.color
+
+            return {
+              color: providedColor || theme.palette.primary.main
+            }
+          }
         }
+      },
+      MuiCssBaseline: {
+        styleOverrides: `
         @font-face {
           font-family: 'Rubik';
-          src: local(Rubik-Regular), url(${fontsToPreload.regular}) format('woff2');
+          src: url(/fonts/Rubik-VariableFont_wght.woff2) format('woff2');
           font-style: normal;
-          font-weight: 400;
+          font-weight: 400 700;
           font-display: swap;
         }
         body{
           font-size: inherit;
         }
       `
-        },
-        MUITypography: {
-          styleOverrides: {
-            root: {
-              fontFamily: 'Rubik, Arial, sans-serif'
-            }
-          }
-        },
-        MuiAvatar: {
-          defaultProps: {
-            slotProps: {
-              img: {
-                referrerPolicy: 'no-referrer',
-                crossOrigin: 'anonymous'
-              }
-            }
+      },
+      MUITypography: {
+        styleOverrides: {
+          root: {
+            fontFamily: 'Rubik, Arial, sans-serif'
           }
         }
       },
-      breakpoints: {
-        values: {
-          mobile: 420,
-          tablet: 640,
-          laptop: 1024,
-          desktop: 1360
+      MuiAvatar: {
+        defaultProps: {
+          slotProps: {
+            img: {
+              referrerPolicy: 'no-referrer',
+              crossOrigin: 'anonymous'
+            }
+          }
         }
       }
-    }),
-    {
-      breakpoints: ['mobile', 'tablet', 'laptop', 'desktop']
+    },
+    breakpoints: {
+      values: {
+        mobile: 420,
+        tablet: 640,
+        laptop: 1024,
+        desktop: 1360
+      }
     }
-  )
-
-  return (
-    <ThemeProvider noSsr theme={theme}>
-      <CssBaseline />
-      <I18nextProvider i18n={i18n}>
-        <AuthProvider>
-          <AppProvider />
-        </AuthProvider>
-      </I18nextProvider>
-    </ThemeProvider>
-  )
-}
+  }),
+  {
+    breakpoints: ['mobile', 'tablet', 'laptop', 'desktop']
+  }
+)
 
 render(
   <StrictMode>
     <Suspense fallback={null}>
-      <Main />
+      <ThemeProvider noSsr theme={theme}>
+        <CssBaseline />
+        <I18nextProvider i18n={i18n}>
+          <AuthProvider>
+            <AppProvider />
+          </AuthProvider>
+        </I18nextProvider>
+      </ThemeProvider>
     </Suspense>
   </StrictMode>,
   document.getElementById('root')
