1d8096ab427990fdc80b6529d15e464746492c31
Gregorio <gre208981@gmail.com>
2026-02-17 02:32:37 -0400

    Refactorize the project actions

--- src/components/ui/drawer/components/ProjectActions.jsx
+++ src/components/ui/drawer/components/ProjectActions.jsx
@@ -1,84 +1,49 @@
 import BarChartIcon from '@mui/icons-material/BarChart'
 import DashboardIcon from '@mui/icons-material/Dashboard'
 import SettingsIcon from '@mui/icons-material/Settings'
-
+import NavAction from '@components/reusable/NavAction'
 import List from '@mui/material/List'
-import DrawerAction from './DrawerAction'
 
-import useLayout from '@hooks/useLayout'
-import useUser from '@hooks/useUser'
-import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+
+const items = [
+  { translation: 'projectActions.dashboard', icon: DashboardIcon, to: '#' },
+  { translation: 'projectActions.metrics', icon: BarChartIcon, to: 'metrics' },
+  { translation: 'projectActions.settings', icon: SettingsIcon, to: 'settings' },
+]
 
 export default function ProjectActions({
   open,
-  column = true,
   noTexts = false
 }) {
   const { t } = useTranslation('ui')
-  const navigate = useNavigate()
-  const { projectId } = useParams()
-  const location = useLocation()
-  const { preferences } = useUser()
-  const { setDrawerOpen } = useLayout()
-
-  const rute = location.pathname?.split(projectId)?.[1]
-  const action = rute === '' ? 'dashboard' : rute
-  const iconSize = noTexts ? 'medium' : 'small'
-
-  const rows = useMemo(
-    () => [
-      {
-        text: t('projectActions.dashboard'),
-        icon: <DashboardIcon fontSize={iconSize} />,
-        // ProjectDashboard component is visible on /projects/:projectId
-        to: '',
-        active: action === 'dashboard'
-      },
-      {
-        text: t('projectActions.metrics'),
-        icon: <BarChartIcon fontSize={iconSize} />,
-        to: '/metrics',
-        active: action === '/metrics'
-      },
-      {
-        text: t('projectActions.settings'),
-        icon: <SettingsIcon fontSize={iconSize} />,
-        to: '/settings',
-        active: action === '/settings'
-      }
-    ],
-    [t, action, iconSize]
-  )
+  const { projectOwner, projectId } = useParams()
 
   return (
     <List
-      className={`flex ${column ? 'flex-column flex-grow' : ''}`}
-      sx={{ gap: '.5rem', p: 0 }}>
-      {rows.map(row => (
-        <DrawerAction
-          onClick={() => {
-            navigate(`/projects/${projectId}${row.to}`, {
-              // the RouteHandler and the LayoutAppBar will use this state
-              state: {
-                fromProject: projectId,
-                projectAction: row.to.replace('/', ''),
-                fromAction: action
-              }
-            })
+      className='flex flex-column flex-grow'
+      sx={{ gap: 1.25 }}
+      disablePadding>
+      {items.map(row => {
+        const { to, translation, ...other } = row
 
-            setDrawerOpen(false)
-          }}
-          key={`projectRow-${row.text}`}
-          text={row.text}
-          icon={row.icon}
-          disabled={row.disabled}
-          active={row.active}
-          open={open}
-          showText={!noTexts}
-        />
-      ))}
+        const link = {
+          to: `/projects/${projectOwner}/${projectId}/${to}`,
+          translation: t(translation),
+          ...other
+        }
+
+        return (
+          <li>
+            <NavAction
+              key={`projectRow-${row.to}`}
+              link={link}
+              showText={open}
+            />
+          </li>
+        )
+      })}
     </List>
   )
 }
