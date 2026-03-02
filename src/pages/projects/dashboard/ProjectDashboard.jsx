7003660b3d4bde0d08dd5446f2fda6aa1d3e4d73
Gregorio <gre208981@gmail.com>
2026-03-01 22:39:52 -0400

    Refactorice the component and move the TasksProvider to be inside the Project provider

--- src/pages/projects/dashboard/ProjectDashboard.jsx
+++ src/pages/projects/dashboard/ProjectDashboard.jsx
@@ -1,7 +1,6 @@
 import Link from '@components/reusable/Link'
 import CircleLoader from '@components/reusable/loaders/CircleLoader'
 import TasksPreviewer from '@components/ui/tasks/TasksPreviewer'
-import TasksProvider from '@context/TasksContext'
 import BreadcrumbIcon from '@mui/icons-material/NavigateNext'
 import Box from '@mui/material/Box'
 import Breadcrumbs from '@mui/material/Breadcrumbs'
@@ -9,83 +8,81 @@ import Chip from '@mui/material/Chip'
 import Typography from '@mui/material/Typography'
 
 // hooks
-import useApp from '@hooks/useApp'
 import useProject from '@hooks/useProject'
-import useUser from '@hooks/useUser'
-import { memo, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+import useLoadResources from '@hooks/useLoadResources'
 
-// utils
-import setPageTitle from '@utils/setPageTitle'
-import { getItem } from '@utils/storage.js'
+const ProjectHeader = ({ projectName, isArchived }) => {
+  const { t } = useTranslation('projects')
+
+  return (
+    <Box
+      display='flex'
+      flexDirection={{ xs: 'column', tablet: 'row' }}
+      gap={2}
+      alignItems='center'
+      mt={2}
+      px={{ xs: 0, tablet: 2 }}
+      justifyContent={{ xs: 'center', tablet: 'start' }}>
+      <Breadcrumbs
+        separator={<BreadcrumbIcon fontSize='small' />}
+        sx={{ width: 'fit-content' }}>
+        <Link underline='hover' color='inherit' to='/projects'>
+          {t('text')}
+        </Link>
+
+        {projectName && (
+          <Typography variant='body2' color='textPrimary'>
+            {projectName}
+          </Typography>
+        )}
+      </Breadcrumbs>
+
+      {isArchived && (
+        <Chip
+          variant='outlined'
+          label={t('archived')}
+          size='small'
+          color='warning'
+          sx={{ alignSelf: { xs: 'flex-start', mobile: 'center' } }}
+        />
+      )}
+    </Box>
+  )
+}
 
 export default function ProjectDashBoard() {
-  const { t } = useTranslation('ui')
-  const { data, updatePreviewer, isArchived, metrics } = useProject()
-  const { isMobile, drawerWidth, isOnlyMobile } = useApp()
-  const { preferences } = useUser()
+  const { t } = useTranslation('projects')
+  const { data, isArchived } = useProject()
 
   const projectName = data?.name
+  const loadingResources = useLoadResources('tasks')
 
-  useEffect(() => {
-    if (projectName) setPageTitle(projectName)
-  }, [projectName])
+  if (!data || loadingResources) {
+    return (
+      <Box
+        className='flex flex-center flex-grow'
+        width='100%'>
+        <CircleLoader text={t('loadingProject')} />
+      </Box>
+    )
+  }
 
   return (
     <Box
       className='flex flex-column flex-grow'
       width='100%'
-      marginLeft='auto'
+      ml='auto'
       justifyContent={!data ? 'center' : 'initial'}>
-      {!data ? (
-        <CircleLoader text={t('projects.loadingProject')} />
-      ) : (
-        <>
-          <Box
-            className={`flex${isMobile ? ' flex-column' : ''}`}
-            gap={2}
-            alignItems={isOnlyMobile ? 'center' : 'center'}
-            mt={2}
-            px={!isMobile ? 2 : 0}
-            justifyContent={isMobile ? 'center' : null}>
-            <Breadcrumbs
-              separator={<BreadcrumbIcon fontSize='small' />}
-              sx={{ width: 'fit-content' }}>
-              <Link underline='hover' color='inherit' to='/projects'>
-                {t('projects.text')}
-              </Link>
+      <ProjectHeader projectName={data.name} isArchived={isArchived} />
 
-              {projectName && (
-                <Typography
-                  variant='body2'
-                  alignSelf='center'
-                  color='textPrimary'>
-                  {projectName}
-                </Typography>
-              )}
-            </Breadcrumbs>
-            {isArchived && (
-              <Chip
-                variant='outlined'
-                label={t('projects.archived')}
-                size='small'
-                color='warning'
-                sx={{ alignSelf: isOnlyMobile ? 'start' : null }}
-              />
-            )}
-          </Box>
-
-          {isArchived && (
-            <Typography className='text-center' my={2} color='warning'>
-              {t('projects.cantUpdateArchived')}
-            </Typography>
-          )}
-
-          <TasksProvider>
-            <TasksPreviewer actualPreview={preferences.previewer || 'list'} />
-          </TasksProvider>
-        </>
+      {isArchived && (
+        <Typography textAlign='center' my={2} color='warning.main'>
+          {t('cantUpdateArchived')}
+        </Typography>
       )}
+
+      <TasksPreviewer />
     </Box>
   )
 }
