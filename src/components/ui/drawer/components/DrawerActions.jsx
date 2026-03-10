48374529323d4f30800e1804ee4094736215dc64
Gregorio <gre208981@gmail.com>
2026-03-10 01:30:30 -0400

    Animate the drawer only if the user clicks an action and the drawer is open

--- src/components/ui/drawer/components/DrawerActions.jsx
+++ src/components/ui/drawer/components/DrawerActions.jsx
@@ -2,13 +2,16 @@ import NavAction from '@components/reusable/NavAction'
 
 import { useLocation, useParams } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import useLayout from '@hooks/useLayout'
 
 import { NAV_LINKS } from '@constants/navigation'
 
-export default function DrawerActions({ open, toggleDrawer }) {
+export default function DrawerActions() {
   const { t } = useTranslation('ui')
   const { projectId } = useParams()
   const { pathname } = useLocation()
+  const { drawerOpen, toggleDrawer: animateDrawer, setDrawerOpen }
+    = useLayout()
 
   const items = []
   for (const link of NAV_LINKS) {
@@ -17,8 +20,13 @@ export default function DrawerActions({ open, toggleDrawer }) {
         <NavAction
           key={link.key}
           link={{ ...link, translation: t(link.translation) }}
-          showText={open}
-          onClick={() => toggleDrawer(false)}
+          showText
+          hideText={!drawerOpen}
+          // run the drawer animation only if the user clicks and the drawer is
+          // closed
+          onClick={() => drawerOpen
+            ? animateDrawer(false)
+            : setDrawerOpen(false)}
           isActive={pathname === link.to}
           className='drawer-action'
           showTooltip
