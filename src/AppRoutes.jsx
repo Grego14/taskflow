328b7b80ef67528352124eec81390d4c2e4d9798
Gregorio <gre208981@gmail.com>
2026-03-29 23:48:25 -0400

    Add the routes provider

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -4,32 +4,35 @@ import { Suspense, lazy } from 'react'
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 
 import Landing from '@pages/landing/Landing'
+import RouteProvider from './context/RouteContext'
 
 const AuthenticatedApp = lazy(() => import('./AuthenticatedApp'))
 const Preview = lazy(() => import('@pages/preview/Preview'))
 
 export default function AppRoutes() {
-  const { notification } = useApp()
+  const { notification, lastRute } = useApp()
 
   return (
     <Router>
       <Suspense fallback={null}>
         <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route
-            path='/preview'
-            element={
-              <Suspense fallback={null}>
-                <Preview />
-              </Suspense>
-            }
-          />
+          <Route element={<RouteProvider />}>
+            <Route path='/' element={<Landing />} />
+            <Route
+              path='/preview'
+              element={
+                <Suspense fallback={null}>
+                  <Preview />
+                </Suspense>
+              }
+            />
 
-          <Route path='/*' element={
-            <Suspense fallback={null}>
-              <AuthenticatedApp />
-            </Suspense>
-          } />
+            <Route path='/*' element={
+              <Suspense fallback={null}>
+                <AuthenticatedApp />
+              </Suspense>
+            } />
+          </Route>
         </Routes>
       </Suspense>
     </Router>
