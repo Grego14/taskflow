a22df33451b7aeb5bebe12f9194dfe24eec49f6b
Gregorio <gre208981@gmail.com>
2026-04-20 20:48:43 -0400

    Increase precision and add a threshold

--- src/utils/tasks/getNewPosition.js
+++ src/utils/tasks/getNewPosition.js
@@ -1,14 +1,20 @@
 const DEFAULT_GAP = 10000
-const PRECISION = 4
+const PRECISION = 8
+const MIN_THRESHOLD = 1
 
 export default function getNewPosition(prevPos = 0, nextPos = 0) {
   let pos
 
   // moving to the top of the list
   if (prevPos === null && nextPos !== null) {
-    // if the first element's position is too low, we bisect the distance to 0 
-    // instead of subtracting to avoid entering negative values
-    pos = nextPos > DEFAULT_GAP ? nextPos - DEFAULT_GAP : nextPos / 2
+    // if there's enough space on the top substract the gap
+    if (nextPos > DEFAULT_GAP) { pos = nextPos - DEFAULT_GAP } 
+
+    // if the space is small but > 1, bisect
+    else if (nextPos > MIN_THRESHOLD) { pos = nextPos / 2 }
+
+    // if we are close to 0 substract a fixed value
+    else { pos = nextPos - 0.1 }
   }
 
   // moving to the bottom of the list
