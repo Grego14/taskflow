0019231f7c37f39e3496d155df78a895e7fab80c
Gregorio <gre208981@gmail.com>
2026-04-20 20:41:58 -0400

    Add new custom routes and the vitest config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from 'node:path'
 import preact from '@preact/preset-vite'
 import { defineConfig } from 'vite'
 import { visualizer } from 'rollup-plugin-visualizer'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
@@ -11,11 +12,11 @@ export default defineConfig({
       filename: 'preact-stats.html',
       gzipSize: true,
       brotliSize: true
-    })
+    }),
+    tsconfigPaths()
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
       '@utils': path.resolve(__dirname, './src/utils'),
       '@components': path.resolve(__dirname, './src/components'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
@@ -26,12 +27,16 @@ export default defineConfig({
       '@context': path.resolve(__dirname, './src/context'),
       '@constants': path.resolve(__dirname, './src/constants'),
       '@stores': path.resolve(__dirname, './src/stores'),
+      '@logic': path.resolve(__dirname, './src/logic'),
+      '@test': path.resolve(__dirname, './src/test'),
+      '@': path.resolve(__dirname, './src'),
       'react': 'preact/compat',
       'react-dom/test-utils': 'preact/compat/test-utils',
       'react-dom': 'preact/compat',
       'react/jsx-runtime': 'preact/jsx-runtime'
     }
   },
+  test: { globals: true, setupFiles: ['./src/test/setup.js'] },
   build: {
     target: 'esnext',
     rollupOptions: {
