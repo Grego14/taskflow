92867ad2cea8f0d6f36f0355ce727d6053dd34ce
Gregorio <gre208981@gmail.com>
2026-05-30 00:59:18 -0400

    add: postcss global data plugin and media queries plugin, update project dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import preact from '@preact/preset-vite'
 import { defineConfig } from 'vite'
 import { visualizer } from 'rollup-plugin-visualizer'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import postcssGlobalData from '@csstools/postcss-global-data';
+import postcssCustomMedia from 'postcss-custom-media'
 
 export default defineConfig({
   plugins: [
@@ -15,6 +17,16 @@ export default defineConfig({
     }),
     tsconfigPaths()
   ],
+  css: {
+    postcss: {
+      plugins: [
+        postcssGlobalData({ 
+          files: [ './src/styles/global/media-queries.css' ]
+        }),
+        postcssCustomMedia()
+      ]
+    }
+  },
   resolve: {
     alias: {
       '@utils': path.resolve(__dirname, './src/utils'),
@@ -29,6 +41,7 @@ export default defineConfig({
       '@stores': path.resolve(__dirname, './src/stores'),
       '@logic': path.resolve(__dirname, './src/logic'),
       '@test': path.resolve(__dirname, './src/test'),
+      '@styles': path.resolve(__dirname, './src/styles'),
       '@': path.resolve(__dirname, './src'),
       'react': 'preact/compat',
       'react-dom/test-utils': 'preact/compat/test-utils',
